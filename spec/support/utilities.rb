2d7ac6933506fed453abaca418388d80ce2ef6e9
mach1010 <jetstream1010@gmail.com>
2014-01-19 22:13:39 -0600

    delete duplcation in utilities.rb

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -13,9 +13,3 @@ RSpec::Matchers.define :have_error_message do |message|
     expect(page).to have_selector('div.alert.alert-error', text: message)
   end
 end
-
-RSpec::Matchers.define :have_error_message do |message|
-  match do |page|
-    expect(page).to have_selector('div.alert.alert-error', text: message)
-  end
-end
